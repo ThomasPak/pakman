5a85d91bbb06a3c4f80ce022ef4f29da68ad09de
Thomas Pak <thomas.pak@linacre.ox.ac.uk>
2019-01-24 21:21:21 +0000

    Add persistent Chaste simulators

--- examples/epithelial-cell-growth/test.sh
+++ examples/epithelial-cell-growth/test.sh
@@ -1,14 +1,26 @@
 #!/bin/bash
 set -euo pipefail
 
+# Chaste dummy simulator
 mpirun -n 4 --oversubscribe \
     pakman rejection chaste-simulator.rej 10 --mpi
 
+# Chaste persistent dummy simulator
 mpirun -n 4 --oversubscribe \
     pakman rejection chaste-persistent-simulator.rej 10 --mpi --persistent
 
-#mpirun -n 4 --oversubscribe \
-#    pakman rejection epithelial-cell-growth.rej 10 --mpi
-#
-#mpirun -n 4 --oversubscribe \
-#    pakman smc epithelial-cell-growth.smc 10 --mpi
+# Chaste simulator ABC rejection
+mpirun -n 4 --oversubscribe \
+    pakman rejection epithelial-cell-growth.rej 10 --mpi
+
+# Chaste simulator ABC SMC
+mpirun -n 4 --oversubscribe \
+    pakman smc epithelial-cell-growth.smc 10 --mpi
+
+# Chaste persistent simulator ABC rejection
+mpirun -n 4 --oversubscribe \
+    pakman rejection epithelial-cell-growth-persistent.rej 10 --mpi --persistent
+
+# Chaste persistent simulator ABC SMC
+mpirun -n 4 --oversubscribe \
+    pakman smc epithelial-cell-growth-persistent.smc 10 --mpi --persistent
