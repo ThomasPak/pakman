88d8d008ad39e75dfc7c0d3bbcfccadef6ed0a71
Thomas Pak <thomas.pak@linacre.ox.ac.uk>
2019-01-24 20:07:48 +0000

    Test Chaste MPI simulators

--- examples/epithelial-cell-growth/test.sh
+++ examples/epithelial-cell-growth/test.sh
@@ -2,7 +2,13 @@
 set -euo pipefail
 
 mpirun -n 4 --oversubscribe \
-    pakman rejection epithelial-cell-growth.rej 10 --mpi
+    pakman rejection chaste-simulator.rej 10 --mpi
 
 mpirun -n 4 --oversubscribe \
-    pakman smc epithelial-cell-growth.smc 10 --mpi
+    pakman rejection chaste-persistent-simulator.rej 10 --mpi --persistent
+
+#mpirun -n 4 --oversubscribe \
+#    pakman rejection epithelial-cell-growth.rej 10 --mpi
+#
+#mpirun -n 4 --oversubscribe \
+#    pakman smc epithelial-cell-growth.smc 10 --mpi
