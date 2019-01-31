1f6ecf9cdfa1eab56af8080b433b6f5c03fe5f80
Thomas Pak <thomas.pak@linacre.ox.ac.uk>
2019-01-31 16:23:37 +0000

    Remove unused functions

--- methods/mpi/mpi_utils.cc
+++ methods/mpi/mpi_utils.cc
@@ -28,15 +28,6 @@ bool iprobe_wrapper(int source, int tag, MPI_Comm comm)
     return static_cast<bool>(flag);
 }
 
-void string_to_buffer(char*& buffer, const std::string& str) {
-
-    // Create buffer to hold string data
-    buffer = new char[str.size() + 1];
-
-    // copy string
-    strcpy(buffer, str.c_str());
-}
-
 void set_terminate_flag(int signal) {
     switch (signal) {
         case SIGINT:
@@ -56,24 +47,6 @@ void set_signal_handler() {
 
     sigaction(SIGINT, &act, nullptr);
     sigaction(SIGTERM, &act, nullptr);
-}
-
-void broadcast_raw_input(const std::string& raw_input) {
-
-    // Broadcast raw input
-    char *buffer;
-    string_to_buffer(buffer, raw_input);
-    Dynamic_Bcast(MPI::COMM_WORLD, buffer, raw_input.size() + 1, MPI::CHAR, MASTER_RANK);
-    delete[] buffer;
-}
-
-void receive_raw_input(std::string& raw_input) {
-
-    // Receive raw input
-    char *buffer;
-    Dynamic_Bcast(MPI::COMM_WORLD, buffer, 0, MPI::CHAR, MASTER_RANK);
-    raw_input.assign(buffer);
-    delete[] buffer;
 }
 
 std::string receive_string(const MPI::Comm& comm, int source, int tag)
--- methods/mpi/mpi_utils.h
+++ methods/mpi/mpi_utils.h
@@ -9,34 +9,13 @@ int get_mpi_comm_world_rank();
 
 bool iprobe_wrapper(int source, int tag, MPI_Comm comm);
 
-void string_to_buffer(char*& buffer, const std::string& str);
 void set_terminate_flag(int signal);
 void set_signal_handler();
-void broadcast_raw_input(const std::string& raw_input);
-void receive_raw_input(std::string& raw_input);
 
 std::string receive_string(const MPI::Comm& comm, int source, int tag);
 int receive_integer(const MPI::Comm& comm, int source, int tag);
 
 std::string receive_string(MPI_Comm comm, int source, int tag);
 int receive_integer(MPI_Comm comm, int source, int tag);
-
-/*** Templates ***/
-template <class T>
-void Dynamic_Bcast(const MPI::Comm& comm, T*& buffer,
-                   int count, const MPI::Datatype& datatype, int root) {
-
-    // Root broadcasts count
-    comm.Bcast(&count, 1, MPI::INT, root);
-
-    // Allocate buffer if not root
-    const int rank = MPI::COMM_WORLD.Get_rank();
-    if ( rank != root )
-        buffer = (T*) new char[count * datatype.Get_size()];
-
-    // Root broadcasts "count" number of elements of datatype
-    comm.Bcast(buffer, count, datatype, root);
-}
-
 
 #endif // MPI_UTILS_H
