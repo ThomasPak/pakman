845082c6d4b26edc38b631b5f39c54400e9584ce
Thomas Pak <thomas.pak@linacre.ox.ac.uk>
2019-02-14 04:13:09 +0000

    Change bracket style

--- methods/debug.cc
+++ methods/debug.cc
@@ -7,7 +7,8 @@
 const int NUM_LEVELS = 20;
 const int STDERR_FILENO = 2;
 
-void print_stacktrace() {
+void print_stacktrace()
+{
 
     void *array[NUM_LEVELS];
     size_t size;
@@ -19,7 +20,8 @@ void print_stacktrace() {
     backtrace_symbols_fd(array, size, STDERR_FILENO);
 }
 
-void handler(int sig) {
+void handler(int sig)
+{
 
     fprintf(stderr, "Error: signal: %s\n", strsignal(sig));
     fprintf(stderr, "Printing stacktrace...");
@@ -29,7 +31,8 @@ void handler(int sig) {
     exit(1);
 }
 
-void set_handlers() {
+void set_handlers()
+{
     signal(SIGABRT, handler);
     signal(SIGSEGV, handler);
     signal(SIGINT, handler);
