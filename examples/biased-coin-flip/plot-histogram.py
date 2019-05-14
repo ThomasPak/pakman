59add63e410df25a251893fb03bb31c27a06ab73
Thomas Pak <thomas.pak@linacre.ox.ac.uk>
2019-05-14 23:27:55 +0100

    Remove shebang line from Python scripts

--- examples/biased-coin-flip/plot-histogram.py
+++ examples/biased-coin-flip/plot-histogram.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python2
 import numpy as np
 import matplotlib.pyplot as plt
 import sys
--- scaling/plot-scaling.py
+++ scaling/plot-scaling.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python2
 import matplotlib.pyplot as plt
 import numpy as np
 import csv
