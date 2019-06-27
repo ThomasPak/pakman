b9988742c4ec3c3d78e19dde26e1aeb975d599b8
Thomas Pak <thomas.pak@linacre.ox.ac.uk>
2019-06-27 12:28:50 +0100

    Implement SIS model example with ABC SMC

---
+++ examples/sis-model/perturbation-pdf.cc
@@ -0,0 +1,61 @@
+#include <string>
+#include <iostream>
+
+#include <math.h>
+
+// Define normal pdf
+double normal_pdf(double mu, double sigma, double x)
+{
+
+    return exp( - (x - mu) * (x - mu) / ( 2.0 * sigma * sigma ) ) /
+                 sqrt( 2.0 * M_PI * sigma * sigma );
+}
+
+int main(int argc, char *argv[])
+{
+    // Process arguments
+    if (argc != 3)
+    {
+        std::cerr << "Usage: " << argv[0] << " STDEV1 STDEV2\n"
+            "Return probability densities of joint normal distributions centered \n"
+            "on given parameters for the perturbed parameter \n"
+            "and with standard deviations STDEV1 and STDEV2\n";
+        return 1;
+    }
+
+    double stdev1 = std::stod(argv[1]);
+    double stdev2 = std::stod(argv[2]);
+
+    // Read generation t and parameter from stdin
+    int t;
+    std::cin >> t;
+
+    double beta_perturbed, gamma_perturbed;
+    std::cin >> beta_perturbed >> gamma_perturbed;
+
+    if (std::cin.fail())
+    {
+        std::cerr << "Error: could not read t, beta_perturbed "
+            "or gamma_perturbed from stdin\n";
+        return 1;
+    }
+
+    // Return normal_pdf for every parameter
+    std::cout.precision(17);
+    double beta, gamma;
+    for (std::cin >> beta >> gamma; !std::cin.eof(); std::cin >> beta >> gamma)
+    {
+        if (std::cin.fail())
+        {
+            std::cerr << "Error: could not read parameter from stdin\n";
+            return 1;
+        }
+
+        double pdf1 = normal_pdf(beta, stdev1, beta_perturbed);
+        double pdf2 = normal_pdf(gamma, stdev2, gamma_perturbed);
+        double pdf = pdf1 * pdf2;
+        std::cout << pdf << std::endl;
+    }
+
+    return 0;
+}
---
+++ examples/sis-model/perturber.cc
@@ -0,0 +1,52 @@
+#include <string>
+#include <iostream>
+#include <random>
+#include <chrono>
+
+int main(int argc, char *argv[])
+{
+    // Process arguments
+    if (argc != 3)
+    {
+        std::cerr << "Usage: " << argv[0] << " STDEV1 STDEV2\n"
+            "Perturb two parameters by drawing "
+            "from normal distributions\n"
+            "centered on the given parameters "
+            "and with standard deviations\n"
+            "STDEV1 and STDEV2, respectively.\n";
+        return 1;
+    }
+
+    double stdev1 = std::stod(argv[1]);
+    double stdev2 = std::stod(argv[2]);
+
+    // Seed random number generator
+    unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+    std::default_random_engine generator(seed);
+
+    // Read generation t and parameter from stdin
+    int t;
+    std::cin >> t;
+
+    double beta, gamma;
+    std::cin >> beta >> gamma;
+
+    if (std::cin.fail())
+    {
+        std::cerr << "Error: could not read t, beta or gamma from stdin\n";
+        return 1;
+    }
+
+    // Perturb parameter
+    std::cout.precision(17);
+    std::normal_distribution<double> distribution1(beta, stdev1);
+    std::normal_distribution<double> distribution2(gamma, stdev2);
+
+    double beta_perturbed = distribution1(generator);
+    double gamma_perturbed = distribution2(generator);
+
+    // Print perturbed parameter
+    std::cout << beta_perturbed << " " << gamma_perturbed << std::endl;
+
+    return 0;
+}
