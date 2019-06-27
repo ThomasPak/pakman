1054ac709f3845ccc60a3bd4603673562d0bae1e
Thomas Pak <thomas.pak@linacre.ox.ac.uk>
2019-06-27 16:12:11 +0100

    Implement cell growth example with ABC SMC

---
+++ examples/epithelial-cell-growth/perturbation-pdf.cc
@@ -0,0 +1,57 @@
+#include <string>
+#include <iostream>
+
+#include <math.h>
+
+// Define normal pdf
+double normal_pdf(double mu, double sigma, double x)
+{
+
+    return exp( - (x - mu) * (x - mu) / ( 2.0 * sigma * sigma ) ) /
+                 sqrt( 2.0 * M_PI * sigma * sigma );
+}
+
+int main(int argc, char *argv[])
+{
+    // Process arguments
+    if (argc != 2)
+    {
+        std::cerr << "Usage: " << argv[0] << " STDEV\n"
+            "Return probability densities of normal distributions centered \n"
+            "on given parameters for the perturbed parameter \n"
+            "and with standard deviation STDEV\n";
+        return 1;
+    }
+
+    double stdev = std::stod(argv[1]);
+
+    // Read generation t and parameter from stdin
+    int t;
+    std::cin >> t;
+
+    double tcycle_perturbed;
+    std::cin >> tcycle_perturbed;
+
+    if (std::cin.fail())
+    {
+        std::cerr << "Error: could not read t or tcycle from stdin\n";
+        return 1;
+    }
+
+    // Return normal_pdf for every parameter
+    std::cout.precision(17);
+    double tcycle;
+    for (std::cin >> tcycle; !std::cin.eof(); std::cin >> tcycle)
+    {
+        if (std::cin.fail())
+        {
+            std::cerr << "Error: could not read parameter from stdin\n";
+            return 1;
+        }
+
+        double pdf = normal_pdf(tcycle, stdev, tcycle_perturbed);
+        std::cout << pdf << std::endl;
+    }
+
+    return 0;
+}
---
+++ examples/epithelial-cell-growth/prior-pdf.cc
@@ -0,0 +1,47 @@
+#include <string>
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    // Process arguments
+    if (argc != 3)
+    {
+        std::cerr << "Usage: " << argv[0] << " TCYCLE_LOW TCYCLE_HIGH\n"
+            "Return probability density of uniform distribution with \n"
+            "lower bound TCYCLE_LOW and upper bound TCYCLE_HIGH "
+            "for given parameter\n";
+        return 1;
+    }
+
+    double tcycle_low = std::stod(argv[1]);
+    double tcycle_high = std::stod(argv[2]);
+
+    if (tcycle_high <= tcycle_low)
+    {
+        std::cerr << "Error: TCYCLE_LOW must be strictly less than TCYCLE_HIGH\n";
+        return 1;
+    }
+
+    // Read parameter from stdin
+    double tcycle;
+    std::cin >> tcycle;
+
+    if (std::cin.fail())
+    {
+        std::cerr << "Error: could not read tcycle from stdin\n";
+        return 1;
+    }
+
+    // Check if tcycle is within bounds
+    std::cout.precision(17);
+    if ( (tcycle_low <= tcycle) && (tcycle < tcycle_high) )
+    {
+        std::cout << 1.0 / (tcycle_high - tcycle_low) << std::endl;
+    }
+    else
+    {
+        std::cout << 0 << std::endl;
+    }
+
+    return 0;
+}
