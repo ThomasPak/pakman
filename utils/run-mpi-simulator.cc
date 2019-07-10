6b3ac3aa4597f5c8a65f0773a39e1cbb36f22d4d
Thomas Pak <thomas.pak@linacre.ox.ac.uk>
2019-07-10 14:15:55 +0100

    Rename run-mpi-simulator and add help message

--- utils/run-mpi-simulator.cc
+++ utils/run-mpi-simulator.cc
@@ -3,17 +3,36 @@
 #include <string>
 #include <chrono>
 
+#include <libgen.h>
+
 #include <mpi.h>
 
-#include "core/Command.h"
 #include "master/MPIWorkerHandler.h"
+
+// Program name
+const char *program_name;
 
 // Global variables
 std::chrono::milliseconds MAIN_TIMEOUT(1);
 bool force_host_spawn = false;
 
+// Help functions
+void help();
+
 int main(int argc, char *argv[])
 {
+    // Set program_name
+    program_name = basename(argv[0]);
+
+    // Check arguments
+    if ((argc == 1)
+            || (std::string(argv[1]).compare("-h") == 0)
+            || (std::string(argv[1]).compare("--help") == 0))
+    {
+        help();
+        return 2;
+    }
+
     // Initialize MPI
     MPI_Init(nullptr, nullptr);
 
@@ -58,4 +77,21 @@ int main(int argc, char *argv[])
 
     // Return error_code
     return error_code;
+}
+
+void help()
+{
+    std::string help_string;
+    help_string += "Usage: ";
+    help_string += program_name;
+    help_string += " <mpi simulator>";
+    help_string += " <mpi simulator args>...";
+    help_string += "\n";
+
+    help_string +=
+"Read epsilon and parameter from stdin, launch mpi simulator,\n"
+"and pass epsilon and parameter as input to mpi simulator.\n"
+"Once the mpi simulator has finished, print output to stdout.\n";
+
+    std::cout << help_string;
 }
