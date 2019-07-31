fe5dacde79b0fcea0d80e4370f3f68f3f1f5d1a3
Thomas Pak <thomas.pak@linacre.ox.ac.uk>
2019-07-31 04:25:57 +0100

    Document help.h

--- src/main/help.h
+++ src/main/help.h
@@ -5,11 +5,30 @@
 
 #include "core/common.h"
 
+/** @file help.h
+ *
+ * This file defines helper functions to display help messages on the
+ * command-line.
+ */
+
+/** @return usage format. */
 std::string usage();
 
+/** @return explanation of general options. */
 std::string general_options();
 
+/** Print an overview of available help messages.
+ *
+ * @param status  exit status.
+ */
 void overview(int status);
+
+/** Print help message when '-h' or '--help' flag is given.
+ *
+ * @param master  master type.
+ * @param controller  controller type.
+ * @param status  exit status.
+ */
 void help(master_t master, controller_t controller, int status);
 
 #endif // HELP_H
